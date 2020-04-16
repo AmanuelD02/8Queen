6084e0946374348e6385e1e6dab446a95a8abc0a
Amanuel Debebe <amanueldebebe1000@gmail.com>
2020-04-16 17:05:49 +0300

    Fixed the bug in the previous version, problem solved with less than one second

--- solution.py
+++ solution.py
@@ -1,8 +1,11 @@
 """ This module returns all the possible solutions to the 8 Queen Problem"""
 
 from itertools import permutations
-
+from pprint import pprint as pp
+import time
 # all possible solution
+
+a = time.time()
 all= permutations(range(8))
 
 # function to check if the given list is corrrect in \ diagon
@@ -11,10 +14,11 @@ def diag1(lst):
     # iterate through all the queens in the list
     for q in range(8):
         Q = lst[q]
-
+        count =0
         for j in range(q+1,8):
+            count+=1
             #print("column:",j , "row:",lst[j],"Queen",Q)  # for checking the solution
-            if lst[j] ==(Q+j):
+            if lst[j] ==(Q+count):
                 return False
     
     return True
@@ -24,10 +28,11 @@ def diag1(lst):
 def diag2(lst):
     for q in range(8):
         Q = lst[q]
-
+        count = 0
         for j in range((q-1),-1,-1):
+            count+=1
             #print("column:",j , "row:",lst[j],"Queen",Q)  # for checking the solution
-            if lst[j] == Q+j:
+            if lst[j] == Q+count:
                 return False
     return True
 
@@ -38,6 +43,9 @@ for lst in all:
         sol.append(lst)
 
 
-print(sol)
+pp(sol)
+print(len(sol))
+print("Time Taken:",time.time()-a)
+
 
        
